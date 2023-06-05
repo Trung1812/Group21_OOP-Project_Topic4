b0f360e1e28bfc30b4b23bb155386a3e7bc25a7f
Trung1812 <phamtrungnhom@gmail.com>
2023-06-05 15:04:42 +0700

    3 sorting algorithms added

---
+++ SortingAlgorithm/src/SortingAlgorithm/SortingAlgorithm.java
@@ -0,0 +1,13 @@
+package SortingAlgorithm;
+
+public class SortingAlgorithm {
+	public static void sort(int[] arr) {
+		return;
+	}
+	public void print(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + '\t');
+		}
+		System.out.print('\n');
+	}
+}
